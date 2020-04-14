937c8018eef473448a2fbf123256aa37dc9039fb
tamzi <tamrefrank@gmail.com>
2020-04-14 09:10:43 +0300

    Adding develop branch and updateing all names to unconfernce App

--- unConference/lib/main.dart
+++ unConference/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'unConference',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'unConference Home Page'),
     );
   }
 }
